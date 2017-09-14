aac189aaa05b61a3adef0bccbb33b496d2d73bbf
jaydeep7 <jaydeep7karena7@gmail.com>
2017-09-14 22:56:48 +0530

    Initial Commit

---
+++ MiniFlipkart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniFlipkart.MvcApplication" Language="C#" %>
---
+++ MiniFlipkart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiniFlipkart.Startup))]
+namespace MiniFlipkart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
